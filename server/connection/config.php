8c424f49e682e1d02d69181e36b7c9e53af41bd1
Nawaf Alsuhaibani <alsuhaibaninawaf@gmail.com>
2020-02-08 15:23:32 -0800

    Update config.php
    Edit connection files

--- server/connection/config.php
+++ server/connection/config.php
@@ -1,13 +1,13 @@
 <?php 
-define('DB_HOST' , 	'localhost');
-define('DBNAME' , 'db_49293153');
-define('DBUSER' , '49293153');
-define('DBPASS' , 'nawaf49293153');
+define('DB_HOST' , 	'DB_HOST');
+define('DBNAME' , 'DBNAME');
+define('DBUSER' , 'DBUSER');
+define('DBPASS' , 'DBPASS');
 
-//define('DB_HOST' , 	'localhost');
-//define('DBNAME' , 'cosc360');
-//define('DBUSER' , 'root');
-//define('DBPASS' , '');
+//define('DB_HOST' , 	'DB_HOST');
+//define('DBNAME' , 'DBNAME');
+//define('DBUSER' , 'DBUSER');
+//define('DBPASS' , 'DBPASS');
 try {
 $conn = new PDO("mysql:host=localhost; dbname=".DBNAME."" , DBUSER, DBPASS);
 	}
